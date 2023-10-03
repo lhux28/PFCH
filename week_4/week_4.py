ce83d521593180cb7cf8fc13eab0488672a251cd
Liwen <lhux28@pratt.edu>
2023-10-02 22:24:36 -0400

    week 4 assignment

---
+++ week_4/week_4.py
@@ -0,0 +1,18 @@
+import csv
+counter = {}
+
+with open('Artworks.csv', 'r') as file:
+    art_read_file = csv.DictReader(file)
+    for artwork in art_read_file:
+        nat_list = artwork["Nationality"].split(" ") #why does it come back as multiple single lists rather than one continuous one?
+        for nat in nat_list:
+            if counter.get(nat) is None:
+                with open(f'/Users/liwen/documents/Github/PFCH/week_4/res/{nat}.csv', 'w') as nat_file:
+                    writer = csv.DictWriter(nat_file, fieldnames=art_read_file.fieldnames)
+                    writer.writeheader()
+                    writer.writerow(artwork)
+                    counter[nat] = True
+            else:
+                with open(f'/Users/liwen/documents/Github/PFCH/week_4/res/{nat}.csv', 'a') as nat_file:
+                    writer = csv.DictWriter(nat_file, fieldnames=art_read_file.fieldnames)
+                    writer.writerow(artwork)
