07b2d049acd84c6202d4160517e814b7a6fd9fc5
Liwen <lhux28@pratt.edu>
2023-10-07 00:31:27 -0400

    week 4 assignment (attempt2)

--- week_4/week_4.py
+++ week_4/week_4.py
@@ -7,12 +7,12 @@ with open('Artworks.csv', 'r') as file:
         nat_list = artwork["Nationality"].split(" ") #why does it come back as multiple single lists rather than one continuous one?
         for nat in nat_list:
             if counter.get(nat) is None:
-                with open(f'/Users/liwen/documents/Github/PFCH/week_4/res/{nat}.csv', 'w') as nat_file:
+                with open(f'res/{nat}.csv', 'w') as nat_file:
                     writer = csv.DictWriter(nat_file, fieldnames=art_read_file.fieldnames)
                     writer.writeheader()
                     writer.writerow(artwork)
                     counter[nat] = True
             else:
-                with open(f'/Users/liwen/documents/Github/PFCH/week_4/res/{nat}.csv', 'a') as nat_file:
+                with open(f'res/{nat}.csv', 'a') as nat_file:
                     writer = csv.DictWriter(nat_file, fieldnames=art_read_file.fieldnames)
                     writer.writerow(artwork)
